8b86905b3f9c58a0f8a0847d1431aeb4e85c71ae
Muthoni77 <gloriamuthoni25@gmail.com>
2023-03-21 15:22:22 +0300

    successful search bar in the navbar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,45 +1,78 @@
-import React from 'react'
-import logo from '../assets/logo.png'
-import { BiPhoneCall } from "react-icons/bi";
+import React from "react";
+import logo from "../assets/logo2.jpg";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { FaUserAlt } from "react-icons/fa";
+import { AiOutlineSearch } from "react-icons/ai";
 import { Outlet, Link } from "react-router-dom";
-const Header = props => (
-  
-    <>
-      <nav className="container mx-auto  flex items-center justify-between  py-1 text-white px-2">
-        <img src={logo} alt="logo" className="w-16 h-16 " />
-        <ul className="flex items-center">
-          <li>
-            <div className="flex items-center mr-2">
-              <BiPhoneCall className="text-[30px] mr-2" />
-              <div>
-                <p>Call to buy</p>
-                <p>1900999</p>
-              </div>
+const Header = (props) => (
+  <>
+    <header class="bg-white">
+      <div class="container mx-auto px-4 py-8 flex items-center">
+        <div class="mr-auto md:w-48 flex-shrink-0 flex">
+          <h1 className="text-blue-500 font-extrabold text-4xl ">ATELLER</h1>
+          <img src={logo} alt="logo" className="h-10 md:h-12 " />
+        </div>
+        <div class="w-full max-w-xs xl:max-w-lg 2xl:max-w-2xl bg-gray-100 rounded-md hidden xl:flex items-center">
+          <select
+            class="bg-transparent uppercase font-bold text-sm p-4 mr-4"
+            name=""
+            id=""
+          >
+            <option>all categories</option>
+          </select>
+          <input
+            class="border-l border-gray-300 bg-transparent font-semibold text-sm pl-4"
+            type="text"
+            placeholder="I'm searching for ..."
+          />
+          <button type="submit" className="ml-auto h-5 px-4 text-gray-500">
+            <div className="">
+              <AiOutlineSearch className="text-[30px] mr-2" />
             </div>
-          </li>
+          </button>
+        </div>
+        <div class="ml-auto md:w-48 hidden sm:flex flex-col place-items-end">
+          <span class="font-bold md:text-xl">8 800 332 65-66</span>
+          <span class="font-semibold text-sm text-gray-400">Support 24/7</span>
+        </div>
 
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/cart">
-              <div className="relative">
-                <AiOutlineShoppingCart className="text-[24px]" />(
-                {props.cartItemNumber})
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+        <nav>
+          <ul className="ml-4 xl:w-48 flex items-center  justify-end ">
+            <li className="ml-5 lg:ml-4 relative inline-block j">
+              <Link to="/sign">
+                <div className="relative">
+                  <FaUserAlt className="text-[24px]" />
+                  <button className="">Accounts </button>
+                  
+                    <div id="myDropdown" class="dropdown-content">
+                      <a href="#home">Home</a>
+                      <a href="#about">About</a>
+                      <a href="#contact">Contact</a>
+                    </div>
+                  
+                </div>
+              </Link>
+            </li>
+            <li className="ml-5 lg:ml-4 relative inline-block">
+              <Link to="/cart">
+                <div className="relative">
+                  <AiOutlineShoppingCart className="text-[24px]" />(
+                  {props.cartItemNumber})
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
 
-      <Outlet />
-    </>
-  );
+    <Outlet />
+  </>
+);
 
+      
 
 export default Header;
-
--- src/Components/product4.jsx
+++ src/Components/product4.jsx
@@ -1,122 +1,49 @@
-// import React from 'react'
-
-// function product4() {
-//   return (
-//     <div>product4</div>
-//   )
-// }
-
-// export default product4
-import React from "react";
-const Product = ({ product, handleAddToCart }) => {
-  const DollarUsd = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "Ksh",
-  });
-  const products = [
-    {
-      id: 1,
-      name: "Product Name",
-      href: "#",
-      price: "9.99",
-      category: "Laptop",
-      imageSrc:
-        "https://images.unsplash.com/photo-1555982105-d25af4182e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
-    },
-    {
-      id: 2,
-      name: "Product Name",
-      href: "#",
-      price: "10.99",
-      category: "Laptop",
-      imageSrc:
-        "https://images.unsplash.com/photo-1508423134147-addf71308178?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
-    },
-    {
-      id: 3,
-      name: "Product Name",
-      href: "#",
-      price: "12.99",
-      category: "Laptop",
-      imageSrc:
-        "https://images.unsplash.com/photo-1449247709967-d4461a6a6103?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
-    },
-    {
-      id: 4,
-      name: "Product Name",
-      href: "#",
-      price: "9.99",
-      category: "Laptop",
-      imageSrc:
-        "https://images.unsplash.com/reserve/LJIZlzHgQ7WPSh5KVTCB_Typewriter.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
-    },
-    {
-      id: 5,
-      name: "Product Name",
-      price: "6.99",
-      href: "#",
-      category: "Laptop",
-      imageSrc:
-        "https://images.unsplash.com/photo-1467949576168-6ce8e2df4e13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
-    },
-    {
-      id: 6,
-      name: "Product Name",
-      href: "#",
-      price: "10.99",
-      category: "Laptop",
-      imageSrc:
-        "https://images.unsplash.com/photo-1544787219-7f47ccb76574?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
-    },
-    {
-      id: 7,
-      name: "Product Name",
-      href: "#",
-      price: "22.99",
-      category: "Laptop",
-      imageSrc:
-        "https://images.unsplash.com/photo-1550837368-6594235de85c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
-    },
-    {
-      id: 8,
-      name: "Product Name",
-      href: "#",
-      price: "19.99",
-      category: "Laptop",
-      imageSrc:
-        "https://images.unsplash.com/photo-1551431009-a802eeec77b1?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&h=400&q=80",
-    },
-  ];
-
-  return (
-    <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-      {products.map((product) => (
-        <a
-          handleAddToCart={handleAddToCart}
-          key={product.id}
-          href={product.href}
-        >
-          <img
-            className="hover:grow hover:shadow-lg"
-            src={product.imageSrc}
-            alt="products"
-          />
-          <div className="pt-3 flex items-center justify-between">
-            <p className="">{product.name}</p>
-          </div>
-          <p className="pt-1 text-red-700 font-bold">
-            {DollarUsd.format(product.price)}
-          </p>
-          <button
-            onClick={() => handleAddToCart(product)}
-            className="bg-gray-300 w-full  rounded-lg py-1 mt-auto mb-2 hover:bg-gray-500 hover:text-white"
-          >
-            Buy now
-          </button>
-        </a>
-      ))}
+ <header class="bg-white">
+       <div class="container mx-auto px-4 py-8 flex items-center">
+         <div class="mr-auto md:w-48 flex-shrink-0">
+             <img src={logo} alt="logo" className="h-8 md:h-10" />
     </div>
-  );
-};
+     <div class="w-full max-w-xs xl:max-w-lg 2xl:max-w-2xl bg-gray-100 rounded-md hidden xl:flex items-center">
+      <select class="bg-transparent uppercase font-bold text-sm p-4 mr-4" name="" id="">
+        <option>all categories</option>
+      </select>
+      <input class="border-l border-gray-300 bg-transparent font-semibold text-sm pl-4" type="text" placeholder="I'm searching for ..."/>
+       <button type="submit" className="">
+                  <div className="ml-auto h-5 px-4 text-gray-500">
+                    <AiOutlineSearch className="text-[30px] mr-2" />
+                  </div>
+                </button>
+    </div>
+     <div class="ml-auto md:w-48 hidden sm:flex flex-col place-items-end">
+      <BiPhoneCall className="text-[30px] mr-2" />
+      <span class="font-bold md:text-xl">8 800 332 65-66</span>
+      <span class="font-semibold text-sm text-gray-400">Support 24/7</span>
+    </div>
+    
+    <nav class="contents">
+           <ul class="ml-4 xl:w-48 flex items-center justify-end">
+ <li className="ml-2 lg:ml-4 relative inline-block">
+          <Link to="/cart">
+            <div className="relative">
+              <FaUserAlt className="text-[24px]" />(
+              {props.cartItemNumber})
+            </div>
+          </Link>
+        </li>
+        <li className="ml-2 lg:ml-4 relative inline-block">
+          <Link to="/cart">
+            <div className="relative">
+              <AiOutlineShoppingCart className="text-[24px]" />(
+              {props.cartItemNumber})
+            </div>
+          </Link>
+        </li>
+         <li>
+          <Link to="/">Home</Link>
+        </li>
+        
 
-export default Product;
+           </ul>
+    </nav>
+
+       </div>
